893fad5ac743d08e058063eaeab357118d218458
NoemiP94 <m_emi94@hotmail.it>
2024-12-16 10:18:21 +0100

    pick up objects

---
+++ Assets/Script/Objects/AnimatorHelper.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimatorHelper : MonoBehaviour
+{
+   public void Destroy()
+    {
+        Destroy(gameObject); //distrugge l'oggetto
+    }
+}
