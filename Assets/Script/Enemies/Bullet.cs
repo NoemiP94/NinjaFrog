eb23ad29d2032ad7bc38b70d36a914ebf9887f82
NoemiP94 <m_emi94@hotmail.it>
2025-01-09 12:58:29 +0100

    remember to fix animator

--- Assets/Script/Enemies/Bullet.cs
+++ Assets/Script/Enemies/Bullet.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public static Bullet instance;
     [SerializeField]
     float speed = 1; //velocità
     Vector3 direction;
+    protected Animator anim;
+    Rigidbody2D rb;
+    bool isDestroyed = false;
 
-
+    private void Awake()
+    {
+        instance = this;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
     public void Init(Vector3 dir)
     {
         direction = dir;
@@ -18,11 +27,24 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (!isDestroyed)
         {
-            //fa danno al giocatore
-            Player.instance.health.TakeDamage();
-        }  
+            if (collision.tag == "Player")
+            {
+                //fa danno al giocatore
+                Player.instance.health.TakeDamage();
+                DestroyBullet();
+            }
+        }
+    }
+
+    private void DestroyBullet()
+    {
+        isDestroyed = true;
+
+        //anim.Play("destroy"); //NON PARTE L'ANIMAZIONE
+
+        Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length);
     }
 
 }
